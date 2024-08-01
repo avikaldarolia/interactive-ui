839180e1028d085e634a7e5973b154771f993097
Avikal Darolia <avikaldarolia7@gmail.com>
2024-08-01 19:06:15 -0400

    Responsiveness for mobile, tablet, desktop.

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,96 @@
+import React, { useState } from "react";
 import bgMobile from "./assets/bg-main-mobile.png";
 import bgDesktop from "./assets/bg-main-desktop.png";
 import logo from "./assets/card-logo.svg";
 import tick from "./assets/icon-complete.svg";
-import { useState } from "react";
 
 export default function App() {
-	const [confirmed, setConfirmed] = useState<Boolean>(false);
+	const [confirmed, setConfirmed] = useState(true);
 	const [name, setName] = useState("");
 	const [cardNumber, setCardNumber] = useState("");
 	const [cvc, setCvc] = useState("");
+	const [year, setYear] = useState("");
+	const [month, setMonth] = useState("");
+	const [errors, setErrors] = useState({
+		name: "",
+		cardNumber: "",
+		cvc: "",
+		year: "",
+		month: "",
+	});
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const newErrors: any = {};
+
+		if (!name) {
+			newErrors.name = "Cardholder name is required";
+		}
+
+		if (!/^\d{16}$/.test(cardNumber.replace(/\s+/g, ""))) {
+			newErrors.cardNumber = "Card number must be 16 digits";
+		}
+
+		if (!/^\d{3}$/.test(cvc)) {
+			newErrors.cvc = "CVC must be 3 digits";
+		}
+
+		if (!/^(0[1-9]|1[0-2])$/.test(month)) {
+			newErrors.month = "Month must be between 01 and 12";
+		}
+
+		if (!/^\d{2}$/.test(year)) {
+			newErrors.year = "Year must be 2 digits";
+		}
+
+		setErrors(newErrors);
+
+		if (Object.keys(newErrors).length === 0) {
+			setConfirmed(true);
+		}
+	};
+
 	return (
 		<>
 			<section>
-				<div className="absolute">
+				<div className="absolute -z-10 w-full">
 					<picture>
 						<source media="(min-width: 1024px)" srcSet={bgDesktop} />
-						<img srcSet={bgMobile} alt="" />
+						<img
+							srcSet={bgMobile}
+							alt=""
+							className="w-full lg:w-1/3 lg:h-screen h-[260px] md:h-[250px]"
+						/>
 					</picture>
 				</div>
-				{/* div */}
-				<div className="grid grid-cols-1 gap-8 lg:grid-cols-2 mx-auto max-w-7xl">
-					<div className="mt-32 mx-5 md:grid md:grid-cols-1">
-						<article className="front-card p-5 flex flex-col justify-between font-semibold">
-							<img className="logo w-12 md:w-20" src={logo} alt="" />
+				<div className="grid grid-cols-1 gap-8 lg:gap-0 lg:grid-cols-2 mx-auto max-w-7xl">
+					<div className="mt-6 md:mt-8 lg:mt-10 mx-5 lg:mx-0 grid grid-cols-1 gap-3 lg:max-w-[480px]">
+						<article className="front-card shadow p-3 md:p-5 flex flex-col justify-between font-semibold">
+							<img className="logo w-10 md:w-20" src={logo} alt="" />
 							<div className="text-white">
-								<h2 className="text-xl md:text-3xl tracking-widest">
-									{cardNumber ? cardNumber : "1234 4567 7885 2342"}
+								<h2 className="md:text-xl lg:text-3xl tracking-wider">
+									{cardNumber ? cardNumber : "1234 4567 7890 1234"}
 								</h2>
-								<ul className="flex items-center md:text-xl justify-between mt-2 md:mt-5 uppercase tracking-wide">
-									<li>{name ? name : "YOUR NAME"}</li>
-									<li>00/00</li>
+								<ul className="flex items-center md:text-xl justify-between mt-1 md:mt-5 uppercase tracking-wide">
+									<li className="text-sm">{name ? name : "Avikal Darolia"}</li>
+									<li className="text-sm">
+										{month ? month : "00"}/{year ? year : "00"}
+									</li>
 								</ul>
 							</div>
 						</article>
-						<article className="back-card md:ml-24 md:-mt-16">
-							<p className="absolute right-10 font-semibold text-sm md:right-12 text-white md:text-xl tracking-widest">
+						<article className="back-card shadow lg:ml-24 lg:mt-32">
+							<p className="absolute right-8 font-semibold text-sm md:right-12 lg:right-10 text-white md:text-xl tracking-widest">
 								{cvc ? cvc : "123"}
 							</p>
 						</article>
 					</div>
-					<div>
+
+					<div className="p-5 md:p-2">
 						{!confirmed && (
 							<form
-								className="flex flex-col justify-center gap-6 h-screen max-w-lg"
-								action="">
+								onSubmit={handleSubmit}
+								className="flex flex-col justify-center gap-6 max-w-lg mx-auto">
 								<div className="">
 									<label htmlFor="cardholder_name">Cardholder Name</label>
 									<input
@@ -53,6 +100,7 @@ export default function App() {
 										onChange={(e) => setName(e.target.value)}
 										required
 									/>
+									{errors.name && <p className="text-red-500">{errors.name}</p>}
 								</div>
 								<div className="">
 									<label htmlFor="card_number">Card Number</label>
@@ -64,29 +112,52 @@ export default function App() {
 										onChange={(e) => setCardNumber(e.target.value)}
 										required
 									/>
+									{errors.cardNumber && (
+										<p className="text-red-500">{errors.cardNumber}</p>
+									)}
 								</div>
 								<div className="flex items-center justify-between gap-6">
-									<div className="flex-1">
-										<label htmlFor="expirty_date">Exp. Date(MM/YY)</label>
-										<input type="month" placeholder="MM YY" required />
+									<div className="flex flex-col">
+										<label htmlFor="expiry_date">Exp. Date(MM/YY)</label>
+										<div className="flex gap-3">
+											<input
+												type="text"
+												placeholder="MM"
+												maxLength={2}
+												value={month}
+												onChange={(e) => setMonth(e.target.value)}
+												required
+											/>
+											<input
+												type="text"
+												placeholder="YY"
+												value={year}
+												onChange={(e) => setYear(e.target.value)}
+												required
+												maxLength={2}
+											/>
+										</div>
+										{errors.month && (
+											<p className="text-red-500">{errors.month}</p>
+										)}
+										{errors.year && (
+											<p className="text-red-500">{errors.year}</p>
+										)}
 									</div>
-									<div className="flex-1">
+									<div className="flex flex-col">
 										<label htmlFor="cvc">CVC</label>
 										<input
-											type="number"
+											type="text"
 											placeholder="Eg. 123"
 											value={cvc}
 											onChange={(e) => setCvc(e.target.value)}
-											max={999}
 											maxLength={3}
 											required
 										/>
+										{errors.cvc && <p className="text-red-500">{errors.cvc}</p>}
 									</div>
 								</div>
-								<button
-									onClick={() => setConfirmed(true)}
-									className="btn w-full"
-									type="submit">
+								<button className="btn w-full" type="submit">
 									Confirm
 								</button>
 							</form>
@@ -106,9 +177,9 @@ function ConfirmationScreen({
 }) {
 	return (
 		<>
-			<div className="flex flex-col justify-center gap-4 h-screen max-w-lg">
+			<div className="flex flex-col mt-[250px] md:mt-[280px] lg:h-screen lg:mt-0 justify-center gap-4 max-w-lg mx-auto">
 				<img src={tick} alt="" className="block mx-auto" />
-				<h1 className="text-slate-600 text-center text-3xl my-6 uppercase">
+				<h1 className="text-slate-600 text-center md:text-3xl lg:my-6 uppercase">
 					Thank you
 				</h1>
 				<p className="text-slate-400 text-center">
@@ -116,7 +187,7 @@ function ConfirmationScreen({
 				</p>
 				<button
 					onClick={() => setConfirmed(false)}
-					className="btn block mx-auto w-full mt-10">
+					className="btn block mx-auto w-full">
 					Continue
 				</button>
 			</div>
